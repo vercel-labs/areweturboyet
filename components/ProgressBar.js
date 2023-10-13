c4cc857a0b67808530f91f258316198cea977ca8
Tim Neutkens <tim@timneutkens.nl>
2023-10-13 11:23:25 +0200

    Remove mention of unit tests

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -4,7 +4,7 @@ export default function ProgressBar({ data }) {
   return (
     <div className="ProgressBar">
       <div className="ProgressBarFill" style={{ width: data.percent + "%" }}>
-        {`${data.passing} of ${data.total} unit tests passing`}
+        {`${data.passing} of ${data.total} integration / end-to-end tests passing`}
       </div>
     </div>
   );
