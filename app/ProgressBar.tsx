f90c97897f17b07968bfda8cec8538de15e75837
Tim Neutkens <tim@timneutkens.nl>
2024-05-21 22:04:50 +0200

    Ensure 100% does not show "0 left"

--- app/ProgressBar.tsx
+++ app/ProgressBar.tsx
@@ -1,17 +1,21 @@
 import Switcher from "./Switcher";
 
-
 export function ProgressBar({ mostRecent, dev }) {
-    return (
-        <div className="flex items-center justify-between px-4 py-2 bg-white shadow-md">
-            <section className="">
-                <div
-                    className="text-lg font-semibold"
-                >
-                    <span className="block sm:inline-block">{mostRecent.passing} of {mostRecent.total} {dev ? `next dev` : 'next build'} tests passing&nbsp;</span><span className="block sm:inline-block">({mostRecent.total - mostRecent.passing} left for 100%)</span>
-                </div>
-            </section>
-            <Switcher />
+  const testsLeft = mostRecent.total - mostRecent.passing;
+  return (
+    <div className="flex items-center justify-between px-4 py-2 bg-white shadow-md">
+      <section className="">
+        <div className="text-lg font-semibold">
+          <span className="block sm:inline-block">
+            {mostRecent.passing} of {mostRecent.total}{" "}
+            {dev ? `next dev` : "next build"} tests passing&nbsp;
+          </span>
+          <span className="block sm:inline-block">
+            ({testsLeft > 0 ? <>{testsLeft} left for 100%</> : "100%"})
+          </span>
         </div>
-    );
+      </section>
+      <Switcher />
+    </div>
+  );
 }
